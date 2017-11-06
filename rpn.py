950169d778e61e3933a184968041ec3c5f79de09
Zach Peterson <zachp@umich.edu>
2017-11-06 09:42:37 -0500

    initial commit with working rpn code

---
+++ rpn.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+def calculate(string):
+        stack = list()
+        for token in string.split():
+            if token == '+':
+                arg1 = stack.pop()
+                arg2 = stack.pop()
+                result = arg1 + arg2
+                stack.append(result)
+            elif token == '-':
+                arg1 = stack.pop()
+                arg2 = stack.pop()
+                result = arg2 - arg1
+                stac.append(result)
+            else:
+                stack.append(int(token))                   
+        print(stack)
+        return stack.pop()
+                
+def main():
+    while True:
+        calculate(input('rpn calc:'))
+
+
+if __name__ == '__main__':
+    main()
+
+
