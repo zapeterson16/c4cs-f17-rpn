29ec512dae574fb9c61b83ea426a5474cdb19513
Zach Peterson <zachp@umich.edu>
2017-11-06 10:43:40 -0500

    added support for exponentiation

--- rpn.py
+++ rpn.py
@@ -13,6 +13,11 @@ def calculate(string):
                 arg2 = stack.pop()
                 result = arg2 - arg1
                 stack.append(result)
+            elif token == '^':
+                arg1 = stack.pop()
+                arg2 = stack.pop()
+                result = arg2**arg1
+                stack.append(result)
             else:
                 stack.append(int(token))                   
         print(stack)
